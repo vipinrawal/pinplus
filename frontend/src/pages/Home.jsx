20a270e79509a813c55fc480ea570d9b66d074df
vipin <86299966+vipinrawal@users.noreply.github.com>
2025-02-25 23:26:59 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { PinData } from "../context/PinContext";
 import { Loading } from "../components/Loading";
 import PinCard from "../components/PinCard";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const { pins, loading } = PinData();
   return (
     <div>
-      {loading ? (
+      
+      {loading ? ( 
         <Loading />
       ) : (
         <div className="container">
+          <div className="searchcontainer">
+            <div className="searchbar">
+              <input
+                type="text"
+                placeholder=" Search for ideas "
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </div>
+        </div>
           <div className="inner-container">
               {pins?.length > 0 ? (
-                pins.map((e) => <PinCard key={e.id || e.someUniqueField} pin={e} />)
+                pins
+                  .filter((pin) =>
+                    pin.title.toLowerCase().includes(searchQuery.toLowerCase())
+                  )
+                  .map((e) => <PinCard key={e.id || e.someUniqueField} pin={e} />)
               ) : (
                 <p className="no-pins">No Pins Yet</p>
               )}
